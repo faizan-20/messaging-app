c08bf466fa12becf85f8182482cc1c84ea3a6d19
faizan-20 <faizan.20a@gmail.com>
2023-11-01 20:41:09 +0530

    authentication added

---
+++ server/auth/auth.js
@@ -0,0 +1,66 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+require('dotenv').config();
+
+const User = require('../models/user');
+
+passport.use(
+    'signup',
+    new LocalStrategy(
+        {
+            usernameField: 'username',
+            passwordField: 'password'
+        },
+        async (username, password, done) => {
+            try {
+                const userExists = await User.findOne({ username });
+                if (userExists) return done(null, false, {message: 'Username alredy exists'});
+                const user = await User.create({ username, password });
+                return done(null, user);
+            } catch (err) {
+                done(err);
+            }
+        }
+    )
+);
+
+passport.use(
+    'login',
+    new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password'
+    },
+    async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username });
+            if (!user) return done(null, flase, {message: 'Incurrect username'});
+            
+            const validate = await user.isValidPassword(password);
+
+            if (!validate) return done(null, false, {message: 'Incorrect password'});
+
+            return done(null, user, {message: 'Logged in successfully'});
+        } catch (err) {
+            return done(err);
+        }
+    }
+    )
+);
+
+passport.use(
+    new JWTstrategy(
+        {
+            secretOrKey: process.env.SECRET_KEY,
+            jwtFromRequest: ExtractJWT.fromHeader('access-token')
+        },
+        async (token, done) => {
+            try {
+                return done(null, token.user);
+            } catch (err) {
+                done(err);
+            }
+        }
+    )
+);
