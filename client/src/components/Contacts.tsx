cd152f49ed67379fb131dccce169435ac6272716
faizan-20 <faizan.20a@gmail.com>
2024-01-23 21:22:02 +0530

    fix controllers and ui update

--- client/src/components/Contacts.tsx
+++ client/src/components/Contacts.tsx
@@ -1,21 +1,47 @@
 import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
 import { AvatarImage } from "./ui/avatar";
+import AddUser from "./AddUser";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type User = {
+  _id: string;
+  username: string;
+  email: string;
+  fullName: string;
+  avatar: string;
+};
 
 type UserProps = {
-  user: {
-    _id: string;
-    username: string;
-    email: string;
-    fullName: string;
-    avatar: string;
-  };
+  user: User;
+};
+
+type Chat = {
+  users: User[];
 };
 
 export default function Contacts({ user }: UserProps) {
+  const [chats, setChats] = useState<User[]>();
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const { data } = await axios.get<Chat[]>("/chat");
+        const results = data.flatMap((x) =>
+          x.users.filter((y) => y._id !== user._id),
+        );
+        setChats(results);
+      } catch (error) {
+        console.error("error fetching chats: ", error);
+      }
+    };
+    fetchChats();
+  }, [user._id]);
+
   return (
     <>
       <div className="w-1/3">
-        <div className="bg-stone-950 h-[12%] flex px-4 py-2 items-center border-2 border-slate-700">
+        <div className="bg-stone-950 h-[12%] flex px-4 py-2 items-center border-2 border-slate-700 justify-around">
           <div className="h-16 w-auto">
             <Avatar>
               <AvatarImage
@@ -26,6 +52,7 @@ export default function Contacts({ user }: UserProps) {
             </Avatar>
           </div>
           <div className="mx-6 font-bold text-xl">{user.fullName}</div>
+          <AddUser />
         </div>
         <div className="bg-gray-800 h-[88%]">Contacts</div>
       </div>
