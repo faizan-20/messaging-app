c17119539b3feb12022d646779c9acff50256529
faizan-20 <faizan.20a@gmail.com>
2024-02-08 20:50:26 +0530

    add chat context

---
+++ client/src/context/ChatProvider.tsx
@@ -0,0 +1,62 @@
+import { User } from "@/components/ContactCard";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+
+export type LatestMessage = {
+  _id: string;
+  sender: User;
+  content: string;
+  chatId: string;
+};
+
+export type ChatType = {
+  _id: string;
+  users: User[];
+  latestMessage?: LatestMessage;
+  selectedChat?: string;
+};
+
+export type ChatContextInterface = {
+  chat: ChatType[];
+  setChat: Dispatch<SetStateAction<ChatType[]>>;
+};
+
+const defaultState = {
+  chat: [
+    {
+      _id: "",
+      users: [],
+      latestMessage: undefined,
+      selectedChat: "",
+    },
+  ],
+  setChat: () => {},
+} as ChatContextInterface;
+
+type ChatProviderProps = {
+  children: ReactNode;
+};
+
+export const ChatContext = createContext(defaultState);
+
+const ChatProvider = ({ children }: ChatProviderProps) => {
+  const [chat, setChat] = useState<ChatType[]>(defaultState.chat);
+
+  return (
+    <ChatContext.Provider value={{ chat, setChat }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
