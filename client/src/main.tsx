0f4044ba43497fa591c681a21ba95291f11c5e3f
faizan-20 <faizan.20a@gmail.com>
2024-02-13 19:24:25 +0530

    bug fix url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,10 +9,10 @@ import Home from "./components/Home.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
 import ChatProvider from "./context/ChatProvider.tsx";
 
-// const PROD = "https://messaging-app-jquv.onrender.com/";
+// const PROD = "https://messaging-app-jquv.onrender.com/api/v1";
 // const DEV = "http://localhost:8000/api/v1";
 
-axios.defaults.baseURL = "https://messaging-app-jquv.onrender.com";
+axios.defaults.baseURL = "https://messaging-app-jquv.onrender.com/api/v1";
 axios.defaults.withCredentials = true;
 
 const router = createBrowserRouter([
